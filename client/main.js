1b7fd176d956cd6636baf30b9b3c42f1683f9957
Jasper O'Leary <jhaazpr@gmail.com>
2020-05-06 11:57:16 -0700

    start class-based script

---
+++ client/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class MomScene {
+    constructor() {
+        this.domContainer = document.getElementById('container');
+        console.log(this.domContainer);
+    }
+}
+
+function main() {
+    let ms = new MomScene();
+
+}
+
+main();
+
