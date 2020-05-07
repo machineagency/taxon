971fa0ce1dcaf5aebb421badff4cf474949f3eee
Jasper O'Leary <jhaazpr@gmail.com>
2020-05-07 16:09:26 -0700

    steal rendering code from example

--- depth_camera/build_mesh.py
+++ depth_camera/build_mesh.py
@@ -1,6 +1,9 @@
 import numpy as np
-import pymesh
+from skimage.measure import block_reduce
 import sys
+from stl import mesh
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
 
 class MeshBuilder:
     def __init__(self):
@@ -15,6 +18,22 @@ class MeshBuilder:
 
     def sample_heightmap(self):
         pass
+
+    def render_meshes(self, meshes):
+        # Create a new plot
+        figure = pyplot.figure()
+        axes = mplot3d.Axes3D(figure)
+
+        # Render the cube faces
+        for m in meshes:
+            axes.add_collection3d(mplot3d.art3d.Poly3DCollection(m.vectors))
+
+        # Auto scale to the mesh size
+        scale = numpy.concatenate([m.points for m in meshes]).flatten(-1)
+        axes.auto_scale_xyz(scale, scale, scale)
+
+        # Show the plot to the screen
+        pyplot.show()
 
     def test(self):
         img = self.load_depth_img()
