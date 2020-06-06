f9f1d8674f8a7944f8be5f3ed0cdc7e00344f2f3
Khang Lee <leekg97@gmail.com>
2020-06-05 23:36:18 -0700

    sass architecture re-org

--- client/ui.js
+++ client/ui.js
@@ -1,6 +1,100 @@
-function create_clicked() {
-   let btn = document.getElementById("create-new-btn");
-   console.log(btn.style.background);
-   img = btn.firstElementChild
-   img.src = ( img.src.includes("_w") ) ? "./img/create.svg" : "./img/create_w.svg";
+/* HELPER FUNCTIONS ------------------------- */
+
+let by_id = (id) => document.getElementById(id);
+
+let css_display_off = (obj) => { if (obj != null) return obj.style.display = "none" }
+let css_display_on = (obj) => { if (obj != null) return obj.style.display = "block" }
+
+let tool_off = (btn, dialog) => {
+   btn.classList.replace("tool-btn-on", "tool-btn-off");
+   css_display_off(dialog);
+}
+
+
+/* GLOBAL DOM ELEMENTS  --------------------- */
+
+let create_btn = by_id("create-new-btn");
+let measure_btn = by_id("measure-btn");
+let instr_btn = by_id("instr-btn");
+
+let obj_list = by_id("objectlist");
+
+let create_dlg = by_id("create-dialog");
+let measure_dlg = by_id("measure-dialog");
+let instr_dlg = by_id("instr-dialog");
+
+let work_env_dlg = by_id("");
+
+let lego_db = {
+   "work-env-obj" : 0,
+   "servo-obj" : 0,
+   "enclosre-obj" : 0,
+   "motor-obj" : 0,
+   "circ-obj" : 0,
+   "stab-obj" : 0
+}
+
+/* FUNCTIONS ------------------------------- */
+
+function toggle_tool(btn) {
+   let classes = btn.classList;
+   let dialog_id = btn.id.replace("btn","dialog");
+   let dialog = document.getElementById(dialog_id);
+
+   if ( classes.contains("tool-btn-on") ) {
+      classes.replace("tool-btn-on", "tool-btn-off");
+      css_display_off(dialog);
+
+   } else {
+      classes.replace("tool-btn-off", "tool-btn-on");
+      css_display_on(dialog);
+
+      switch(btn.id) {
+         case "create-new-btn":
+            tool_off(measure_btn, measure_dlg);
+            tool_off(instr_btn, instr_dlg);
+            break;
+
+         case "measure-btn":
+            tool_off(create_btn, create_dlg);
+            tool_off(instr_btn, instr_dlg);
+            break;
+
+         case "instr-btn":
+            tool_off(create_btn, create_dlg);
+            tool_off(measure_btn, measure_dlg);
+            break;
+      }
+   }
+}
+
+function add_object(obj) {
+
+   // Swtich statement for creating object using three.js
+
+   let divider = document.createElement("div")
+   divider.classList.add("line");
+
+   let item = document.createElement("button");
+   item.classList.add("list-item");
+   item.type = "button";
+
+   let desc = document.createElement("p");
+   let index = ++lego_db[obj.id];
+   desc.innerHTML = obj.lastElementChild.innerHTML + " " + index;
+   item.appendChild(desc);
+
+   obj_list.appendChild(divider);
+   obj_list.appendChild(item);
+}
+
+function open_obj_property(lego) {
+
+   // switch 
+
+}
+
+function nav_back(current) {
+   css_display_off(current);
+   css_display_on(create_dlg);
 }
