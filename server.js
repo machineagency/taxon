41b746762ba953fca057cde2a8e82f7d9c9520b6
Jasper O'Leary <jhaazpr@gmail.com>
2020-11-23 18:02:29 -0800

    seed databse

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const ObjectID       = mongodb.ObjectID;
 let port = process.env.PORT || 3000; // set our port
 app.use(bodyParser.json()); // for parsing application/json
 app.use(express.static(__dirname + '/client')); // set the static files location /public/img will be /img for users
+const DO_SEED_DATABASE = true;
+const MACHINE_DIR = './machine_programs/';
 
 // connect to db ===========================================
 const url = 'mongodb://127.0.0.1:27017/strange-machine';
@@ -21,6 +23,9 @@ mongoClient.connect(url, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database');
         const db = client.db('strange-machine');
+        if (DO_SEED_DATABASE) {
+            seedDatabase(db);
+        }
         attachRoutesWithDBAndStart(db);
     })
     .catch(error => console.error(error));
@@ -123,4 +128,32 @@ let attachRoutesWithDBAndStart = (db) => {
         exports = module.exports = app;
     });
 }
+
+// extras ===========================================
+let seedDatabase = (db) => {
+    db.dropDatabase()
+    .then((_) => {
+        fs.readdir(MACHINE_DIR, (err, files) => {
+            if (err) {
+                throw err;
+            }
+            files.forEach((filename) => {
+                let fullFilename = MACHINE_DIR + filename;
+                fs.readFile(fullFilename, (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                    let machineObj = JSON.parse(data);
+                    db.collection('machines').insertOne(machineObj)
+                    .catch((error) => {
+                        throw error;
+                    });
+                });
+            });
+        });
+    })
+    .catch((error) => {
+        throw error;
+    });
+};
 
