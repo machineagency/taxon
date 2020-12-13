6085e5f885782c01d4f43c5c0d2dc9398d7bd7a2
Jasper O'Leary <jhaazpr@gmail.com>
2020-12-13 09:54:54 -0800

    can load stuff

--- client/ModelAnalyzer.js
+++ client/ModelAnalyzer.js
@@ -1,9 +1,104 @@
+'use strict';
+
+import * as THREE from './build/three.module.js';
+import { STLLoader } from './build/STLLoader.js';
+import { OrbitControls } from './build/OrbitControls.js';
+import { TransformControls } from './build/TransformControls.js';
+import { Line2 } from './build/lines/Line2.js';
+import { LineGeometry } from './build/lines/LineGeometry.js';
+import { LineMaterial } from './build/lines/LineMaterial.js';
+
+import { StrangeGui } from './StrangeGui.js';
+import { TestPrograms } from './TestPrograms.js';
 
 class ModelAnalyzer {
+
+    constructor() {
+        this.modelContainerDom = document.getElementById('model-container');
+        this.drawingContainerDom = document.getElementById('drawing-container');
+        this.renderModelScene = this.__makeModelSceneRenderer();
+        this.renderDrawingScene = this.__makeDrawingSceneRenderer();
+        this.makeLoadStlPromise('./pikachu.stl');
+    }
+
+    makeLoadStlPromise = (filepath) => {
+        let loadPromise = new Promise(resolve => {
+            let loader = new STLLoader();
+            let stlMesh;
+            return loader.load(filepath, (stlGeom) => {
+                let material = new THREE.MeshBasicMaterial({
+                    color: 0xffffff,
+                    wireframe: true
+                });
+                stlMesh = new THREE.Mesh(stlGeom, material);
+                stlMesh.scale.set(10, 10, 10);
+                stlMesh.isLoadedStl = true;
+                this.modelScene.add(stlMesh);
+                resolve(stlMesh);
+            }, undefined, (errorMsg) => {
+                console.log(errorMsg);
+            });
+        });
+        return loadPromise;
+    };
+
+    __makeModelSceneRenderer() {
+        let domElement = this.modelContainerDom;
+        let scene = new THREE.Scene();
+        let aspect = domElement.offsetWidth / domElement.offsetHeight;
+        let viewSize = 50;
+        let camera = new THREE.OrthographicCamera(-viewSize * aspect, viewSize * aspect,
+            viewSize, -viewSize, -1000, 10000);
+        camera.zoom = 0.1;
+        camera.updateProjectionMatrix();
+        camera.frustumCulled = false;
+        camera.position.set(500, 500, 500); // I don't know why this works
+        camera.lookAt(scene.position);
+        scene.add(camera);
+        scene.background = new THREE.Color(0x000000);
+        let gridHelper = new THREE.GridHelper(2000, 50, 0xe5e6e8, 0x444444);
+        scene.add(gridHelper);
+        let controls = new OrbitControls(camera, domElement);
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 0.8;
+        controls.panSpeed = 0.8;
+        controls.keys = [65, 83, 68];
+        scene.orbitControls = controls;
+
+        let mcRenderer = new THREE.WebGLRenderer({ antialias: true });
+        const {left, right, top, bottom, width, height} =
+            this.modelContainerDom.getBoundingClientRect();
+        mcRenderer.setPixelRatio(window.devicePixelRatio);
+        mcRenderer.setSize(width - 2, height - 2);
+        domElement.appendChild(mcRenderer.domElement);
+
+        this.modelScene = scene;
+        this.modelCamera = camera;
+        this.modelControls = controls;
+
+        let renderModelScene = () => {
+            controls.update();
+            mcRenderer.render(scene, camera);
+        };
+        return renderModelScene;
+    }
+
+    __makeDrawingSceneRenderer() {
+        // TODO
+    }
 }
 
 const main = () => {
-    console.log('Hello world');
+    const ma = new ModelAnalyzer();
+    console.log(ma);
+    let animate = () => {
+        let maxFramerate = 20;
+        setTimeout(() => {
+            requestAnimationFrame(animate);
+        }, 1000 / maxFramerate);
+        ma.renderModelScene();
+    };
+    animate();
 };
 
 main();
