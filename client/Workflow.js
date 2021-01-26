084e8c908cf770b41a0a2139e57ac113b9dd2834
Jasper O'Leary <jhaazpr@gmail.com>
2021-01-26 12:31:33 -0800

    currying execution

--- client/Workflow.js
+++ client/Workflow.js
@@ -1,12 +1,34 @@
 'use strict';
 
 class Workflow {
+
+    static EndFunctionName = 'END_FUNCTION';
+
     constructor(progText) {
         this.progText = progText;
-        this.statements = this.splitProgTextIntoStatements(progText);
+        this.statements = this.__splitProgTextIntoStatements(progText);
+        this.progOutermostFn = this.testCurry();
+        this.profCurrFn = this.testCurry();
     }
 
-    splitProgTextIntoStatements(progText) {
+    initializeProgram() {
+    }
+
+    step() {
+        // TODO: UI step
+        if (this.profCurrFn.name === Workflow.EndFunctionName) {
+            console.warn('End of program.');
+            return;
+        }
+        this.profCurrFn = this.profCurrFn();
+    }
+
+    reset() {
+        // TODO: UI reset too
+        this.profCurrFn = this.testCurry();
+    }
+
+    __splitProgTextIntoStatements(progText) {
         let lines = progText.split(';')
                         .map(line => line.trim())
                         .filter(line => line.length > 0);
@@ -14,6 +36,28 @@ class Workflow {
     }
 
     testCurry() {
+        let baseProg = "let v = 42; let mySum = v + 5; console.log(mySum)";
+        let f0 = () => {
+            let f1 = () => {
+                let f2 = () => {
+                    console.log(mySum);
+                    let endFunction = () => {};
+                    Reflect.defineProperty(endFunction, 'name', {
+                        value: Workflow.EndFunctionName,
+                        writeable: false
+                    });
+                    console.log('did logging');
+                    return endFunction;
+                };
+                let mySum = v + 5;
+                console.log('did v + 5');
+                return f2;
+            };
+            let v = 42
+            console.log('defined v');
+            return f1;
+        };
+        return f0;
     }
 }
 
