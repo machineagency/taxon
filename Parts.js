029ffe5bc4c673f557aed510051cb68fdf3413ca
Jasper O'Leary <jhaazpr@gmail.com>
2021-03-07 19:40:22 -0800

    stubs for parts

---
+++ Parts.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import * as THREE from './build/three.module.js';
+import { STLLoader } from './build/STLLoader.js';
+import { OrbitControls } from './build/OrbitControls.js';
+import { TransformControls } from './build/TransformControls.js';
+import { StrangeGui } from './StrangeGui.js';
+
+class PartAssembly {
+}
+
+class Part {
+}
+
+class Joint {
+    constructor(parentPart, childPart, jointType, offset) {
+    }
+}
+
+class Powertrain {
+    constructor() {
+    }
+}
+
+class PartsCompiler {
+    constructor() {
+    }
+
+    compile() {
+    }
+}
+
