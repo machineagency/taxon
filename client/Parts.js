e2b1b137310f2fa9a48e15ba50a54a9271bff53e
Jasper O'Leary <jhaazpr@gmail.com>
2021-03-09 10:16:43 -0800

    add motor boxes

--- client/Parts.js
+++ client/Parts.js
@@ -12,13 +12,18 @@ class PartsAssembly {
 
     constructor(parentScene, bounds) {
         this.parentScene = parentScene;
+        this.parts = [];
         this.rootMeshGroup = new THREE.Group();
+        this.parentScene.scene.add(this.rootMeshGroup);
+        this.parentScene.partsAssembly = this;
         this.bounds = bounds;
         this.renderBounds();
     }
 
-    addMeshGroupToScene(group) {
-        this.parentScene.addSceneObjectDirectly(group);
+    addPart(part) {
+        console.assert(part.meshGroup !== undefined);
+        this.rootMeshGroup.add(part.meshGroup);
+        this.parts.push(part);
     }
 
     renderBounds() {
@@ -34,11 +39,74 @@ class PartsAssembly {
         meshGroup.add(wireSegments);
         meshGroup.name = 'bounds';
         let geometries = [geom, edgesGeom];
-        this.addMeshGroupToScene(meshGroup);
+        this.parentScene.addSceneObjectDirectly(meshGroup);
+        this.parentScene.camera.lookAt(meshGroup.position);
+        this.parentScene.camera.zoom = 2;
+        this.parentScene.camera.updateProjectionMatrix();
     }
 }
 
 class Part {
+    static DefaultColor = 0x222222;
+    static MotorColor = 0xffed90;
+
+    constructor(partProgObj, partsAssembly) {
+        this.partsAssembly = partsAssembly;
+        this.name = partProgObj.name;
+        this.isMotor = !!partProgObj.isMotor;
+        // TODO: read dimensions/position from implementation or explicit
+        this.position = new THREE.Vector3(partProgObj.position.x,
+                                          partProgObj.position.y,
+                                          partProgObj.position.z);
+        this.dimensions = {
+            width: 25,
+            height: 25,
+            length: 25
+        }
+        this.render();
+        this.partsAssembly.addPart(this);
+    }
+
+    render() {
+        const triPerFace = 2;
+        let geom = new THREE.BoxBufferGeometry(this.dimensions.width,
+                                               this.dimensions.height,
+                                               this.dimensions.length,
+                        triPerFace, triPerFace, triPerFace);
+        let edgesGeom = new THREE.EdgesGeometry(geom);
+        let edgesMaterial = new THREE.LineBasicMaterial({
+            color: PartsAssembly.boundsColor
+        });
+        let meshColor;
+        if (this.isMotor) {
+            meshColor = Part.MotorColor;
+        }
+        else {
+            meshColor = Part.DefaultColor;
+        }
+        let meshMaterial = new THREE.MeshLambertMaterial({
+            transparent: true,
+            opacity: 0.05,
+            color: meshColor
+        });
+        let mesh = new THREE.Mesh(geom, meshMaterial);
+        let wireSegments = new THREE.LineSegments(edgesGeom, edgesMaterial);
+        this.meshGroup = new THREE.Group();
+        this.meshGroup.add(wireSegments);
+        this.meshGroup.add(mesh);
+        this.meshGroup.name = this.name;
+        let geometries = [geom, edgesGeom];
+        this.meshGroup.position.copy(this.position);
+    }
+
+    removeFromScene() {
+        if (this.meshGroup) {
+            this.partsAssembly.strangeScene.scene.remove(this.meshGroup);
+        }
+    }
+
+    loadDetailFromImplementation(implementationObj) {
+    }
 }
 
 class Joint {
@@ -59,6 +127,8 @@ class PartsCompiler {
         let pObj = JSON.parse(prog);
         let bounds = pObj.bounds
         let pa = new PartsAssembly(window.strangeScene, bounds);
+        let motors = pObj.parts.filter(p => p.isMotor);
+        motors.forEach(m => new Part(m, pa));
         return prog;
     }
 }
