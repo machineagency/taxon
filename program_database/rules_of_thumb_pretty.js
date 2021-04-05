85a315fe6cee4711187c2fc33e65e940db342285
Jasper O'Leary <jhaazpr@gmail.com>
2021-04-05 10:34:32 -0700

    add pre-uglify rot file

---
+++ program_database/rules_of_thumb_pretty.js
@@ -0,0 +1,8 @@
+/* This file is for rules of thumb implementations before uglifying them
+ * and putting them in JSON files in the rules_of_thumb/ directory.
+ */
+
+/* Filtering */
+
+/* Actions */
+
