0215692317067baaf9c5e63df39ed2cbecf62a00
Jasper O'Leary <jhaazpr@gmail.com>
2021-04-06 12:54:48 -0700

    first pass dep rot

--- program_database/rules_of_thumb/we_larger_than.js
+++ program_database/rules_of_thumb/we_larger_than.js
@@ -1,1 +1,15 @@
-(machine) => { return true; };
+(machine, depValue) => {
+    try {
+        // Assuming dependent value is a string in the form
+        // "[width, height, length]"
+        let dimArray = JSON.parse(depValue);
+        let [minWidth, minHeight, minLength] = dimArray;
+        let we = machine.metrics.workEnvelope;
+        return we.width >= minWidth
+            && we.height >= minHeight
+            && we.length >= minLength;
+    }
+    catch (e) {
+        return false;
+    }
+};
