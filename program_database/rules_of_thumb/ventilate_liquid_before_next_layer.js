e66a264e5aa448e251918c71a1c44b0eeda059f9
Jasper O'Leary <jhaazpr@gmail.com>
2021-04-06 10:54:01 -0700

    update ventilate rot to match new metaprogramming style

--- program_database/rules_of_thumb/ventilate_liquid_before_next_layer.js
+++ program_database/rules_of_thumb/ventilate_liquid_before_next_layer.js
@@ -1,13 +1,11 @@
 (action, store) => {
     // todo fix bnased on new syntax
     try {
-        let methods = getMethods(action);
-        if (getMethodName(methods[0]) === 'fanOn') {
+        if (getMethodName(action) === 'fanOn') {
             store['highestLayerWet'] = false;
         }
-        console.log(getMethodArgs(methods[0]));
-        if (getMethodName(methods[0]) === 'moveTo'
-            && getMethodArgs(methods[0])[1] !== undefined) {
+        if (getMethodName(action) === 'moveTo'
+            && getMethodArgs(action)[1] !== undefined) {
             if (store['highestLayerWet']) {
                 console.error('Must dry the previous layer first.');
                 return false;
