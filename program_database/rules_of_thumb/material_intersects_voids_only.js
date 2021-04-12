251ba735a6211cb46641795bfbe61d6615cd6921
Jasper O'Leary <jhaazpr@gmail.com>
2021-04-12 14:04:06 -0700

    void cop-out

--- program_database/rules_of_thumb/material_intersects_voids_only.js
+++ program_database/rules_of_thumb/material_intersects_voids_only.js
@@ -1,2 +1,6 @@
 (action, store) => {
+    // Note: we un-implemented this rule of thumb because it was not working
+    // right. Having a material intersect a void alone is no guarantee that
+    // it cannot intersect any non-void (solid) parts of the blocks. In the
+    // future, it would be easier to describe solids rather than voids.
     return true; };
