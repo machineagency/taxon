b9f5105849c805471d52bb2c49eda42ebf3b0837
Jasper O'Leary <jhaazpr@gmail.com>
2021-05-20 12:34:21 -0700

    promise-style choose point Note that when we finally get pointObj from the spawned process, use the return to invoke any then calls in functions that may be waiting on the result of choosePoint, e.g. placeAt in this commit. Note that we also suspend stepping until the promise is fulfulled.

--- client/CameraProjector.js
+++ client/CameraProjector.js
@@ -5,20 +5,22 @@ import * as THREE from './build/three.module.js';
 class CameraProjector {
     static baseUrl = 'http://localhost:3000/rpc/';
 
-    constructor() {
+    constructor(workflow) {
+        this.workflow = workflow;
     }
 
     async choosePoint() {
         // TODO: pass work envelope dimensions
         let rpcUrl = CameraProjector.baseUrl.concat('choosePoint');
-        console.log(rpcUrl);
+        this.workflow.grayStepButton();
         let response = await fetch(rpcUrl, {
             method: 'GET'
         });
         if (response.ok) {
             let json = await response.json();
             let pointObj = json.results;
-            console.log(pointObj);
+            this.workflow.ungrayStepButton();
+            return pointObj;
         }
         else {
             console.error(response.text);
