310a632d46d1eb693ca2148639284a53b6802c02
Jasper O'Leary <jhaazpr@gmail.com>
2021-09-22 12:40:21 -0700

    in server, localhost -> docker process name

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const MACHINE_DIR = './program_database/';
 
 // connect to db ===========================================
 console.log('Looking for MongoDB instance...');
-const url = 'mongodb://127.0.0.1:27017/';
+const url = 'mongodb://taxon-mongo:27017/';
 mongoClient.connect(url, { useUnifiedTopology: true })
     .then(client => {
         console.log('...connected to the database.');
