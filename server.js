6923b488445da9f4abb1229d072e5a26847dffac
Jasper O'Leary <jhaazpr@gmail.com>
2021-09-22 11:59:55 -0700

    remove pythonshell functionality for now this was causing a lot of issues with docker

--- server.js
+++ server.js
@@ -17,31 +17,6 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(express.static(__dirname + '/client')); // set the static files location /public/img will be /img for users
 const DO_SEED_DATABASE = true;
 const MACHINE_DIR = './program_database/';
-const shell = new ps.PythonShell('./cp_interpreter.py', {});
-
-/* Keep references to the name and Express response object for the current
- * RPC and set the shell.on handler once only, using a lookup table that
- * checks the name of the RPC and handles it appropriately. This is because
- * we cannot unbind shell.on handlers and so cannot set them in routes. */
-shell.currRpcResponse = undefined;
-shell.currRpcName = '';
-shell.on('message', (message) => {
-    if (shell.currRpcName === 'choosePoint') {
-        let xyPair = message;
-        let parsedPair = xyPair.split(',').map(s => parseInt(s));
-        shell.currRpcResponse.status(200).json({
-            results: {
-                x: parsedPair[0],
-                y: parsedPair[1]
-            }
-        });
-        shell.currRpcResponse = undefined;
-        shell.currRpcName = '';
-    }
-    else {
-        console.log(`PC --> ${message}`);
-    }
-});
 
 // connect to db ===========================================
 console.log('Looking for MongoDB instance...');
