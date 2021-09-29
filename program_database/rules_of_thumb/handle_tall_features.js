614d74374a134999063992ff72cf9efdb11aa64f
Jasper O'Leary <jhaazpr@gmail.com>
2021-09-29 13:32:41 -0700

    implement handle tall features rot

--- program_database/rules_of_thumb/handle_tall_features.js
+++ program_database/rules_of_thumb/handle_tall_features.js
@@ -1,2 +1,46 @@
-(machine) => { return machine; };
+(machine) => {
+    const hasXZPlatform = (blocks) => {
+        const platforms = blocks.filter(block => block.attributes.isPlatform);
+        const whetherEachPlatformIsXZ = platforms.map((platform) => {
+            return !platform.attributes.isImmobile
+                && hasXZActuatingAncestor(platform, blocks);
+        });
+        return whetherEachPlatformIsXZ.reduce((p0, p1) => p0 || p1, false);
+    };
+    const hasXZActuatingAncestor = (thisBlock, allBlocks) => {
+        const thisBlockAncestor = getBlockAncestor(thisBlock, allBlocks);
+        if (!thisBlockAncestor) {
+            return false;
+        }
+        else {
+            const ancestorIsXZ = thisBlockAncestor.actuationAxes.includes('x')
+                || thisBlockAncestor.actuationAxes.includes('z');
+            if (ancestorIsXZ) {
+                return true;
+            }
+            else {
+                return hasXZActuatingAncestor(thisBlockAncestor, allBlocks);
+            }
+        }
+    };
+    // Assumption: a block can have at most one ancestor
+    const getBlockAncestor = (childBlock, allBlocks) => {
+        return allBlocks.find((block) => {
+            if (block.connections) {
+                const currBlockChildrenNames = block.connections.map((cxn) => {
+                    return cxn.child;
+                });
+                return currBlockChildrenNames.includes(childBlock.name);
+            }
+            return false;
+        });
+    };
+
+    try {
+        return !hasXZPlatform(machine.blocks);
+    }
+    catch (e) {
+        return false;
+    }
+};
 
